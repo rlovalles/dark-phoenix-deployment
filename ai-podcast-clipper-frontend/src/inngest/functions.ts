2b5b47b6cf3492841fd5155a40e530bc331b7a43
Rebecca Ovalles <rebovalles@gmail.com>
2026-06-14 21:25:45 -0600

    youtube url ingestion to inngest

--- ai-podcast-clipper-frontend/src/inngest/functions.ts
+++ ai-podcast-clipper-frontend/src/inngest/functions.ts
@@ -14,9 +14,10 @@ export const processVideo = inngest.createFunction(
   },
   { event: "process-video-events" },
   async ({ event, step }) => {
-    const { uploadedFileId } = event.data as {
+    const { uploadedFileId, youtubeUrl } = event.data as {
       uploadedFileId: string;
       userId: string;
+      youtubeUrl?: string;
     };
 
     try {
